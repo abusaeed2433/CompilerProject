877554484834deb5d5ed833a374267c725b947bb
Abu Saeed <abusaeed2433@gmail.com>
2023-09-22 18:26:00 +0600

    comment done

--- lex.yy.c
+++ lex.yy.c
@@ -282,21 +282,21 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 7
-#define YY_END_OF_BUFFER 8
-static yyconst short int yy_accept[19] =
+#define YY_NUM_RULES 8
+#define YY_END_OF_BUFFER 9
+static yyconst short int yy_accept[21] =
     {   0,
-        0,    0,    0,    0,    8,    7,    7,    5,    6,    5,
-        5,    2,    1,    4,    3,    1,    1,    0
+        7,    7,    0,    0,    9,    8,    7,    8,    5,    6,
+        5,    5,    7,    2,    1,    4,    3,    1,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
+        1,    4,    1,    1,    1,    1,    5,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -322,37 +322,37 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[5] =
+static yyconst int yy_meta[6] =
     {   0,
-        1,    1,    2,    3
+        1,    2,    2,    3,    4
     } ;
 
-static yyconst short int yy_base[23] =
+static yyconst short int yy_base[25] =
     {   0,
-       15,   14,    0,    0,   17,   20,    2,   20,   20,    0,
-        0,   20,   14,   20,   20,   13,   20,   20,    6,   11,
-       11,    9
+        0,    0,    5,    0,   22,   23,    0,    7,   23,   23,
+        0,    0,    0,   23,   18,   23,   23,   17,   23,   23,
+       17,   14,   14,   12
     } ;
 
-static yyconst short int yy_def[23] =
+static yyconst short int yy_def[25] =
     {   0,
-       19,   19,   18,    3,   18,   18,   18,   18,   18,   20,
-       21,   18,   22,   18,   18,   22,   18,    0,   18,   18,
-       18,   18
+       20,    1,   20,    3,   20,   20,   21,   20,   20,   20,
+       22,   23,   21,   20,   24,   20,   20,   24,   20,    0,
+       20,   20,   20,   20
     } ;
 
-static yyconst short int yy_nxt[25] =
+static yyconst short int yy_nxt[29] =
     {   0,
-        8,    9,   10,   11,   12,   13,    6,    6,    6,   16,
-       16,   16,   15,   14,   17,   17,   18,    7,    7,    5,
-       18,   18,   18,   18
+        6,    7,    7,    6,    8,    9,    9,   10,   11,   12,
+       14,   15,   18,   18,   18,   18,   17,   16,   13,   19,
+       19,   20,    5,   20,   20,   20,   20,   20
     } ;
 
-static yyconst short int yy_chk[25] =
+static yyconst short int yy_chk[29] =
     {   0,
-        3,    3,    3,    3,    7,    7,   19,   19,   19,   22,
-       22,   22,   21,   20,   16,   13,    5,    2,    1,   18,
-       18,   18,   18,   18
+        1,    1,    1,    1,    1,    3,    3,    3,    3,    3,
+        8,    8,   24,   24,   24,   24,   23,   22,   21,   18,
+       15,    5,   20,   20,   20,   20,   20,   20
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -636,13 +636,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 19 )
+				if ( yy_current_state >= 21 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 20 );
+		while ( yy_base[yy_current_state] != 23 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -701,9 +701,14 @@ YY_RULE_SETUP
 case 7:
 YY_RULE_SETUP
 #line 85 "code.l"
+{}
+	YY_BREAK
+case 8:
+YY_RULE_SETUP
+#line 87 "code.l"
 ECHO;
 	YY_BREAK
-#line 707 "lex.yy.c"
+#line 712 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(COMMENT):
 	yyterminate();
@@ -996,7 +1001,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 19 )
+			if ( yy_current_state >= 21 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1031,11 +1036,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 19 )
+		if ( yy_current_state >= 21 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 18);
+	yy_is_jam = (yy_current_state == 20);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1590,7 +1595,7 @@ int main()
 	return 0;
 	}
 #endif
-#line 85 "code.l"
+#line 87 "code.l"
 
 
 int yywrap(){
